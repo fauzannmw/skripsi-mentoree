243b65fd11babb69fb62261ab85041bb7d4c613d
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-12-01 10:42:17 +0700

    fix type error next-auth

---
+++ lib/authOptions.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID ?? "",
+    //   clientSecret: process.env.GITHUB_SECRET ?? "",
+    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    //@ts-ignore
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return (
+          profile.email_verified && profile.email.endsWith("@student.ub.ac.id")
+        );
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
