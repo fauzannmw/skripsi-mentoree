5f72985ba1d213d286dba7312342c883f4e3657c
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-12-01 11:02:35 +0700

    Update authOptions.ts

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -4,10 +4,6 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID ?? "",
-    //   clientSecret: process.env.GITHUB_SECRET ?? "",
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
