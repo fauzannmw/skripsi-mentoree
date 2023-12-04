2a55574165b83e244f43b528c2e72dee8fe2b486
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-12-04 15:43:15 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default async function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="justify-center inline-block max-w-lg text-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
+        <h1 className={title()}>Welcome&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Mentoree&nbsp;</h1>
+        {/* <br />
         <h1 className={title()}>
           websites regardless of your design experience.
         </h1>
         <h2 className={subtitle({ class: "mt-4" })}>
           Beautiful, fast and modern React UI library.
-        </h2>
+        </h2> */}
       </div>
     </section>
   );
