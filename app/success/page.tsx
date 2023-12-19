02e2f36763b15888daa335fad3bd8a3915ea7f7f
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-12-19 23:21:36 +0700

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,13 +1,7 @@
-import { getMentorByNim, getProfileUser } from "@/server/get_action";
-
-// @ts-ignore
-export default async function CheckoutPage({ params }) {
-  const mentorData = (await getMentorByNim(params.id)).detail;
-  const userData = (await getProfileUser()).detail;
-
-  console.log(mentorData);
-
+export default async function SuccessPage() {
   return (
-    <div className="flex flex-col justify-between gap-2 select-none"></div>
+    <div className="flex flex-col justify-between gap-2 select-none">
+      Selamat transaksi anda Berhasil!
+    </div>
   );
 }
