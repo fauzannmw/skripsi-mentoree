9cc8a7905b2ac9950ae73771c84dba9edee59863
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-12-24 14:05:57 +0700

    Update page.tsx

--- app/admin/transaction/page.tsx
+++ app/admin/transaction/page.tsx
@@ -27,7 +27,9 @@ export default async function AdminTransactionPage() {
             <IoMdPrint />
           </Button>
         </div>
-        <Table transactions={data as TransactionExtends} />
+        {/* @ts-ignore */}
+        <Table transactions={data} />
+        {/* <Table transactions={data as TransactionExtends} /> */}
         <div className="flex flex-col items-center w-full px-4 py-2 space-y-2 text-sm text-gray-500 sm:justify-between sm:space-y-0 sm:flex-row">
           <p className="flex">
             Menampilkan&nbsp;
